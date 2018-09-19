3a4b94ae76562c5eead047c099448f3a3f9f4d49
Sergei Kotlyachkov <kotl@2x2.cc>
2018-09-19 02:56:10 +0000

    no more mongodb

--- scripts/compile_mongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source cs_dirs
-
-cd
-wget https://fastdl.mongodb.org/src/mongodb-src-r4.0.2.tar.gz
-tar -zxf mongodb-src-r4.0.2.tar.gz
-cd mongodb-src-r4.0.2
-sudo pip install -r buildscripts/requirements.txt
