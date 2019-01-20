1f6854e700ccfe29e400a3016b7b1bdab9ac2bf9
Sergei Kotlyachkov <kotl@users.noreply.github.com>
2019-01-20 14:44:51 -0500

    Fix missing comma

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -20,7 +20,7 @@ var sites = [
  { urls: ["http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700"], recursive: false},
  { urls: ["http://fonts.googleapis.com/css?family=Product+Sans"], recursive: false},
  { urls: ["http://fonts.googleapis.com/css?family=Roboto:300,400,500,700"], recursive: false},
- { urls: ["http://fonts.googleapis.com/icon?family=Material+Icons+Extended"], recursive: false}
+ { urls: ["http://fonts.googleapis.com/icon?family=Material+Icons+Extended"], recursive: false},
 
  { urls: ["http://google.com/favicon.ico"], recursive: false},
   { urls: [
